11df3d58188f3e3d3cd216890cc27420d6033ed4
Liyynk <92435494+Liyynk@users.noreply.github.com>
2025-01-19 23:18:50 +0800

    add exp_-x (#1)

--- src/execute.py
+++ src/execute.py
@@ -3,8 +3,12 @@ from NlImpl import NLOperation
 def run(config = 'template/config.ini'):
     nlf = NLOperation(config)
     nlf.verilog_emit()
+    nlf.testbench_emit()
 
     if nlf.VIS:
         nlf.vis_dif()
 
     return
+
+if __name__ == "__main__":
+    run(config = 'template/config.ini')
